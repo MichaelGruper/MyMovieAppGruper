2742e64340d9a1470e83b0fd6bffd0278c6f169a
Michael Gruper <sarge6411@icloud.com>
2025-01-17 07:52:18 -0600

    Initial Commit

---
+++ MyMovieAppGruperTests/MyMovieAppGruperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieAppGruperTests.swift
+//  MyMovieAppGruperTests
+//
+//  Created by MICHAEL GRUPER on 1/17/25.
+//
+
+import Testing
+@testable import MyMovieAppGruper
+
+struct MyMovieAppGruperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
